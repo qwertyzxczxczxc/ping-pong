1a0560a28b27bb495ffb54a3f8ca52a704c886f7
qwertyzxczxczxc <105007767+qwertyzxczxczxc@users.noreply.github.com>
2022-05-19 19:40:17 +0300

    version 2
    добавлен отскок от ракетки

--- qwe.py
+++ qwe.py
@@ -50,26 +50,49 @@ racket2 = Player('qwe2.png', 520, 200, 4, 50, 150)
 ball = GameSprite('qwe1.png', 200, 200, 4, 50, 50)
 speed_x = 3
 speed_y = 3
+
+font.init()
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('Игрок 1 проиграл', True,(180, 0, 0))
+
+font2 = font.Font(None, 35)
+lose2 = font1.render('Игрок 2 проиграл', True,(180, 0, 0))
+
+
+
  
 while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
   
-   if finish != True:
-       window.fill(back)
-       racket1.update_l()
-       racket2.update_r()
-       ball.rect.x += speed_x
-       ball.rect.y += speed_y
-       if ball.rect.y > win_height-50 or ball.rect.y < 0:
-           speed_y *= -1
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2, (200, 200))
  
-       racket1.reset()
-       racket2.reset()
-       ball.reset()
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
  
  
-   display.update()
-   clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
  
+
